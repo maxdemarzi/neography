86095cd72b1e256b8a5d9aaec70e993d9d5851e6
nickflux <nick@bolsovernetworks.com>
2012-03-19 17:11:23 +0000

    Update to config to allow for different locations for cypher and gremlin plugins

--- lib/neography/config.rb
+++ lib/neography/config.rb
@@ -1,17 +1,19 @@
 module Neography
   class Config 
-    class << self; attr_accessor :protocol, :server, :port, :directory, :log_file, :log_enabled, :logger, :max_threads, :authentication, :username, :password end
+    class << self; attr_accessor :protocol, :server, :port, :directory, :cypher_path, :gremlin_path, :log_file, :log_enabled, :logger, :max_threads, :authentication, :username, :password end
 
-    @protocol = 'http://'
-    @server = 'localhost'
-    @port = 7474 
-    @directory = ''
-    @log_file = 'neography.log'
-    @log_enabled = false
-    @logger = Logger.new(@log_file) if @log_enabled
-    @max_threads = 20
+    @protocol       = 'http://'
+    @server         = 'localhost'
+    @port           = 7474 
+    @directory      = ''
+    @cypher_path    = '/cypher'
+    @gremlin_path   = '/ext/GremlinPlugin/graphdb/execute_script'
+    @log_file       = 'neography.log'
+    @log_enabled    = false
+    @logger         = Logger.new(@log_file) if @log_enabled
+    @max_threads    = 20
     @authentication = {}
-    @username = nil
-    @password = nil
+    @username       = nil
+    @password       = nil
   end
 end
