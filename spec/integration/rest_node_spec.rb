d230abfd1ba1389a2a41a37e8ca11b7766190953
Max De Marzi <maxdemarzi@hotmail.com>
2012-04-14 02:05:07 +0000

    making sure

--- spec/integration/rest_node_spec.rb
+++ spec/integration/rest_node_spec.rb
@@ -46,7 +46,7 @@ describe Neography::Rest do
       new_node["data"]["name"].should == "Max"
       new_node["data"]["age"].should == 31
       new_node_id = new_node["self"].split('/').last
-      existing_node = @neo.create_unique_node(index_name, key, value, {"age" => 31, "name" => "Max"})
+      existing_node = @neo.create_unique_node(index_name, key, value, {"age" => 39, "name" => "Thomas"})
       existing_node["self"].split('/').last.should == new_node_id
       existing_node["data"]["name"].should == "Max"
       existing_node["data"]["age"].should == 31
