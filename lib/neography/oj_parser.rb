2a2333cccd3807065483431b0e3b6bdac3229c76
Max De Marzi <maxdemarzi@hotmail.com>
2012-06-04 21:59:29 -0500

    switching httparty to excon

--- lib/neography/oj_parser.rb
+++
@@ -1,8 +0,0 @@
-class OjParser < HTTParty::Parser
- 
-  protected 
-    def json
-      #Oj::Doc.parse(body)
-      Oj.load(body)
-    end
-end
--- lib/neography/rest.rb
+++ lib/neography/rest.rb
@@ -1,9 +1,8 @@
 module Neography
   
   class Rest
-    include HTTParty
     
-    attr_accessor :protocol, :server, :port, :directory, :cypher_path, :gremlin_path, :log_file, :log_enabled, :logger, :max_threads, :authentication, :username, :password, :parser
+    attr_accessor :protocol, :server, :port, :directory, :cypher_path, :gremlin_path, :log_file, :log_enabled, :logger, :max_threads, :authentication, :username, :password
 
       def initialize(options=ENV['NEO4J_URL'] || {})
         init = {:protocol       => Neography::Config.protocol, 
@@ -18,7 +17,6 @@ module Neography
                 :authentication => Neography::Config.authentication,
                 :username       => Neography::Config.username,
                 :password       => Neography::Config.password,
-                :parser         => Neography::Config.parser
                 }
 
         unless options.respond_to?(:each_pair)
@@ -47,7 +45,6 @@ module Neography
         @max_threads    = init[:max_threads]
         @authentication = Hash.new
         @authentication = {"#{init[:authentication]}_auth".to_sym => {:username => init[:username], :password => init[:password]}} unless init[:authentication].empty?
-        @parser         = init[:parser]
       end
 
       def configure(protocol, server, port, directory)
@@ -479,15 +476,14 @@ module Neography
      end
 
       def evaluate_response(response)
-        code = response.code
-        body = response.body
+        code = response.status
         case code 
           when 200 
             @logger.debug "OK" if @log_enabled
-            response.parsed_response
+            Oj.load(response.body)
           when 201
             @logger.debug "OK, created #{body}" if @log_enabled
-            response.parsed_response
+            Oj.load(response.body)
           when 204  
             @logger.debug "OK, no content returned" if @log_enabled
             nil
@@ -504,19 +500,19 @@ module Neography
       end
 
        def get(path,options={})
-          evaluate_response(HTTParty.get(configuration + URI.encode(path), options.merge!(@authentication).merge!(@parser)))
+          evaluate_response(Excon.get(configuration + URI.encode(path), options.merge!(@authentication)))
        end
 
        def post(path,options={})
-          evaluate_response(HTTParty.post(configuration + URI.encode(path), options.merge!(@authentication).merge!(@parser)))
+          evaluate_response(Excon.post(configuration + URI.encode(path), options.merge!(@authentication)))
        end
 
        def put(path,options={})
-          evaluate_response(HTTParty.put(configuration + URI.encode(path), options.merge!(@authentication).merge!(@parser)))
+          evaluate_response(Excon.put(configuration + URI.encode(path), options.merge!(@authentication)))
        end
 
        def delete(path,options={})
-          evaluate_response(HTTParty.delete(configuration + URI.encode(path), options.merge!(@authentication).merge!(@parser)))
+          evaluate_response(Excon.delete(configuration + URI.encode(path), options.merge!(@authentication)))
        end
 
       def get_id(id)
