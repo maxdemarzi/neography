802802c6d0fcba4f6ccfe1c1b414125017c8afa1
Max De Marzi <maxdemarzi@hotmail.com>
2012-07-30 12:03:16 -0500

    dealing with JSON parsing issues

--- lib/neography/multi_json_parser.rb
+++ lib/neography/multi_json_parser.rb
@@ -2,8 +2,28 @@ class MultiJsonParser < HTTParty::Parser
 
   protected
 
+    # I know this looks pretty ugly, but we have issues with Neo4j returning true, false,
+    # plain numbers and plain strings, which is considered by some JSON libraries to be
+    # invalid JSON, but some consider it perfectly fine.
+    # This ugly hack deals with the problem.  Send me a Pull Request if you
+    # come up with a nicer solution... please!
+    #
     def json
-      MultiJson.load(body)
+      begin
+        MultiJson.load(body)
+      rescue MultiJson::DecodeError
+        case
+          when body == "true"
+            true
+          when body == "false"
+            false
+           when body.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+            Float(body)
+          else
+            body[1..-2]
+        end
+
+      end
     end
 
 end
