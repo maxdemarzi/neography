a9687eb3307241ed373bea60b0d3db0fb9a53b44
Max De Marzi <maxdemarzi@hotmail.com>
2012-11-27 15:51:02 -0600

    getting rid of find_and_require_user_defined_code, closing #68

--- lib/neography.rb
+++ lib/neography.rb
@@ -1,18 +1,3 @@
-def find_and_require_user_defined_code
-  extensions_path = ENV['neography_extensions'] || "~/.neography"
-  extensions_path = File.expand_path(extensions_path)
-  if File.exists?(extensions_path)
-    Dir.open extensions_path do |dir|
-      dir.entries.each do |file|
-        if file.split('.').size > 1 && file.split('.').last == 'rb'
-          extension = File.join(File.expand_path(extensions_path), file) 
-          require(extension) && puts("Loaded Extension: #{extension}")
-        end
-      end
-    end
-  end
-end
-
 require 'cgi'
 require 'httparty'
 require 'json'
@@ -46,8 +31,6 @@ require 'neography/node'
 require 'neography/relationship'
 
 require 'neography/railtie' if defined? Rails::Railtie
-
-find_and_require_user_defined_code
 
 module Neography
 
