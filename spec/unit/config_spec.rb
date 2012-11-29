ed6e9604982b0840399cb5b5d57996e37491e84b
Max De Marzi <maxdemarzi@hotmail.com>
2012-11-29 00:35:18 -0600

    wonder if this works on travis

--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Neography
   describe Config do
 
-    subject(:config)
+    subject{Config.new}
 
     context "defaults" do
 
@@ -37,7 +37,7 @@ module Neography
           :password       => nil,
           :parser         => { :parser => MultiJsonParser }
         }
-        config.to_hash.should == expected_hash
+        subject.to_hash.should == expected_hash
       end
 
     end
