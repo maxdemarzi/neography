c450858ab3afe86432a79bc3d20172ef32fb53d2
Peter Boling <peter.boling@gmail.com>
2013-01-18 15:53:41 -0500

    Provides a named initialization hook so people depending on the gem can ensure pre-requisite configuration is complete prior to initialization of neography itself

--- lib/neography/railtie.rb
+++ lib/neography/railtie.rb
@@ -3,8 +3,17 @@ require 'rails'
 module Neography
   class Railtie < Rails::Railtie
 
+    # To add an initialization step from your Railtie to Rails boot process, you just need to create an initializer block:
+    # See: http://api.rubyonrails.org/classes/Rails/Railtie.html
+    initializer 'neography.configure' do
+      # Provides a hook so people implementing the gem can do this in a railtie of their own:
+      #   initializer "my_thing.neography_initialization", before: 'neography_railtie.configure_rails_initialization' do
+      #     require 'my_thing/neography'
+      #   end
+    end
+
     rake_tasks do
-      load "neography/tasks.rb"
+      load 'neography/tasks.rb'
     end
   end
 end
