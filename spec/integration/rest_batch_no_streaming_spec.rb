ee25406abe4f584d1840f9bdb2fb238175c04e13
Radu Kopetz <radu.kopetz@ecairn.com>
2014-04-15 15:52:30 +0200

    Bump to 1.4.2

---
+++ spec/integration/rest_batch_no_streaming_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Neography::Rest do
+  before(:each) do
+    @neo = Neography::Rest.new
+  end
+  
+  describe "no streaming" do
+  
+    it "can send a 1000 item batch" do
+      commands = []
+      1000.times do |x|
+        commands << [:create_node, {"name" => "Max " + x.to_s}]
+      end
+      batch_result = @neo.batch_no_streaming *commands
+      batch_result.first["body"]["data"]["name"].should == "Max 0"
+      batch_result.last["body"]["data"]["name"].should == "Max 999"
+    end
+
+    it "can send a 5000 item batch" do
+      commands = []
+      5000.times do |x|
+        commands << [:get_node, 0]
+      end
+      batch_result = @neo.batch_no_streaming *commands
+      batch_result.first["body"]["self"].split('/').last.should == "0"
+      batch_result.last["body"]["self"].split('/').last.should == "0"
+    end
+
+    it "can send a 20000 item batch" do
+      commands = []
+      20000.times do |x|
+        commands << [:create_node, {"name" => "Max " + x.to_s}]
+      end
+      batch_result = @neo.batch_no_streaming *commands
+      batch_result.first["body"]["data"]["name"].should == "Max 0"
+      batch_result.last["body"]["data"]["name"].should == "Max 19999"
+    end
+  end
+
+end
