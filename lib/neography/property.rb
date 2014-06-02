f97c834ad8a628f2df78e1c738082a24b88f23fb
Jannis Hermanns <jannis@gmail.com>
2014-06-02 15:01:34 +0200

    Add #reset_properties to Node and Relationship
    Uses the already implemented reset_properties REST endpoint.

--- lib/neography/property.rb
+++ lib/neography/property.rb
@@ -2,9 +2,7 @@ module Neography
   module Property
 
     def [](key)
-      key = key.to_sym
-      return unless respond_to?(key)
-      @table[key]
+      @table[key.to_sym]
     end
 
     def []=(key, value)
@@ -12,38 +10,69 @@ module Neography
       k_str = key.to_s
       if value.nil?
         unless @table[key].nil?
-          if self.is_a? Neography::Node
+          if node?
             neo_server.remove_node_properties(self.neo_id, [k_str])
           else
             neo_server.remove_relationship_properties(self.neo_id, [k_str])
           end
         end
+        remove_ostruct_member(key)
       else
-        if self.is_a? Neography::Node
+        if node?
           neo_server.set_node_properties(self.neo_id, {k_str => value})
         else
           neo_server.set_relationship_properties(self.neo_id, {k_str => value})
         end
-        new_ostruct_member(key) unless self.respond_to?(key)
+        new_ostruct_member(key, value)
       end
-      @table[key] = value
     end
 
-    def new_ostruct_member(name)
+    # Set many properties at once and only issue one http
+    # request and update the node/relationship instance on the fly.
+    def reset_properties(hash)
+      hash.each do |key, value|
+        add_or_remove_ostruct_member(key, value)
+      end
+      if node?
+        neo_server.reset_node_properties(self.neo_id, hash)
+      else
+        neo_server.reset_relationship_properties(self.neo_id, hash)
+      end
+    end
+
+    def add_or_remove_ostruct_member(name, value)
+      if value.nil?
+        remove_ostruct_member(name)
+      else
+        new_ostruct_member(name, value)
+      end
+    end
+
+    def new_ostruct_member(name, value)
       name = name.to_sym
+      @table[name] = value
       unless self.respond_to?(name)
         meta = class << self; self; end
         meta.send(:define_method, name) { @table[name] }
-        meta.send(:define_method, "#{name}=") do |value|
-          self[name.to_sym] = value
+        meta.send(:define_method, "#{name}=") do |new_value|
+          self[name.to_sym] = new_value
         end
       end
       name
     end
 
+    def remove_ostruct_member(name)
+      @table.delete(name.to_sym)
+      meta = class << self; self; end
+      names = [name, "#{name}="].map(&:to_sym)
+      names.each do |n|
+        meta.send(:remove_method, n) if self.respond_to?(n)
+      end
+    end
+
     def method_missing(method_sym, *arguments, &block)
       if (method_sym.to_s =~ /=$/) != nil
-        new_ostruct_member(method_sym.to_s.chomp("="))
+        new_ostruct_member(method_sym.to_s.chomp("="), *arguments)
 
         # We just defined the getter/setter above, but we haven't actually
         # applied them yet.
@@ -55,6 +84,10 @@ module Neography
 
     def attributes
       @table.keys
+    end
+
+    def node?
+      self.is_a?(Neography::Node)
     end
 
   end
--- lib/neography/property_container.rb
+++ lib/neography/property_container.rb
@@ -4,7 +4,7 @@ module Neography
 
     def initialize(hash=nil)
       @table = {}
-      if hash
+      unless hash.nil?
         if hash["self"] # coming from REST API
           @neo_id = hash["self"].split('/').last
           data = hash["data"]
@@ -12,16 +12,15 @@ module Neography
           @neo_id = hash.neo_id
           data = Hash[*hash.attributes.collect{|x| [x.to_sym, hash.send(x)]}.flatten]
         elsif hash["data"] # coming from CYPHER
-          @neo_id = hash["data"].first.first["self"].split('/').last  
+          @neo_id = hash["data"].first.first["self"].split('/').last
           data = hash["data"].first.first["data"]
         end
       else
         data = []
-      end      
-      
+      end
+
       for k,v in data
-        @table[k.to_sym] = v
-        new_ostruct_member(k)
+        new_ostruct_member(k.to_sym, v)
       end
     end
 
