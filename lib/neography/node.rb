b0e6815dd3970c5cda3779975aa87634f83ce24d
Jannis Hermanns <jannis@gmail.com>
2014-06-02 14:57:20 +0200

    Get rid of class self block for easier readability

--- lib/neography/node.rb
+++ lib/neography/node.rb
@@ -8,36 +8,32 @@ module Neography
 
     attr_accessor :neo_server
 
-    class << self
-      def create(props = nil, db = Neography::Rest.new)
-        raise ArgumentError.new("syntax deprecated") if props.is_a?(Neography::Rest)
+    def self.create(props = nil, db = Neography::Rest.new)
+      raise ArgumentError.new("syntax deprecated") if props.is_a?(Neography::Rest)
 
-        node = self.new(db.create_node(props))
+      node = self.new(db.create_node(props))
+      node.neo_server = db
+      node
+    end
+
+    def self.create_unique(index, key, value, props = nil, db = Neography::Rest.new)
+      raise ArgumentError.new("syntax deprecated") if props.is_a?(Neography::Rest)
+
+      node = self.new(db.create_unique_node(index, key, value, props))
+      node.neo_server = db
+      node
+    end
+
+    def self.load(node, db = Neography::Rest.new)
+      raise ArgumentError.new("syntax deprecated") if node.is_a?(Neography::Rest)
+      node = node.first if node.kind_of?(Array)
+      node = db.get_node(node) if (node.to_s.match(/^\d+$/) or node.to_s.split("/").last.match(/^\d+$/))
+      if node
+        node = self.new(node)
         node.neo_server = db
-        node
       end
-
-      def create_unique(index, key, value, props = nil, db = Neography::Rest.new)
-        raise ArgumentError.new("syntax deprecated") if props.is_a?(Neography::Rest)
-
-        node = self.new(db.create_unique_node(index, key, value, props))
-        node.neo_server = db
-        node
-      end
-
-      def load(node, db = Neography::Rest.new)
-        raise ArgumentError.new("syntax deprecated") if node.is_a?(Neography::Rest)
-        node = node.first if node.kind_of?(Array)
-        node = db.get_node(node) if (node.to_s.match(/^\d+$/) or node.to_s.split("/").last.match(/^\d+$/))
-        if node
-          node = self.new(node)
-          node.neo_server = db
-        end
-        node
-      end
-
-      #alias_method :new, :create
-    end    
+      node
+    end
 
     def find(*args)
       node = self.new
